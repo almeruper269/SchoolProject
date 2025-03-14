549fb8787b2961864fad2d60841918d2974c1766
almeruper269 <almeruper269@gmail.com>
2025-03-14 06:48:49 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+   int age = 18;
+   if (age >= 18) {
+       printf("You are eligible to vote.\n");
+   } else {
+       printf("You are not eligible to vote.\n");
+   }
+   return 0;
+}
