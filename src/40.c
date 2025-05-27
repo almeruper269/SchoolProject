b68f400a50e49745a151c1e80a9f1fdc13ac77dc
almeruper269 <almeruper269@gmail.com>
2025-05-27 23:25:01 +0000

    Add 40.c

---
+++ src/40.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int array[5] = {10, 20, 30, 40, 50};
+    
+    for (i = 0; i < 5; i++) {
+        printf("%d ", array[i]);
+    }
+    
+    return 0;
+}
